99498cda25f6bd3aa63cfeebabafce4dd5348cb6
izielin <misiopar@wp.pl>
2020-03-28 13:46:26 +0100

    improve appearance of create $ upload book form template

--- books/forms.py
+++ books/forms.py
@@ -13,3 +13,8 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'publisher', 'category', 'condition', 'amount', 'price', 'description']
+
+    def __init__(self, *args, **kwargs):
+        super(BookForm, self).__init__(*args, **kwargs)
+        self.fields['category'].widget.attrs['class'] = 'form-control'
+        self.fields['condition'].widget.attrs['class'] = 'form-control'
